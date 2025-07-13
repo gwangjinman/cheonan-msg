6035c1762fb27249bc2e8f10026d910bc3905ec1
기헌 김 <dktkwkak@naver.com>
2025-07-13 21:33:35 +0900

    25-07-13

--- src/app/qr-dialog.tsx
+++ src/app/qr-dialog.tsx
@@ -28,18 +28,21 @@ export default function QRDialog() {
                     <DialogHeader>
                         <DialogTitle
                         className="text-white text-center">
-                            QR코드를 스캔하여 카카오톡 친구추가
+                            QR코드로 카카오톡 친구추가
                         </DialogTitle>
                         <DialogDescription
-                        className="text-white text-center">
-                            아래 QR코드를 카카오톡에서 스캔하세요.
+                        className="text-white text-center mt-3">
+                            아래 QR코드를 카카오톡에서 스캔하세요.<br />
+                            모바일 환경이라면 QR코드를 길게 눌러<br />
+                            열기 버튼을 선택해 주세요.
+
                         </DialogDescription>
                     </DialogHeader>
                     <div className="flex flex-col items-center justify-center py-4">
                         <Image
                             src="/qr.png"
-                            width={300}
-                            height={300}
+                            width={500}
+                            height={500}
                             alt="카카오톡 QR코드"
                             className="rounded-lg
                                     border-2 border-gray-700 bg-black"
